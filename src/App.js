61535d036a378f494e49c15c1fe0855bf85b4de8
Swastik Singh <swastikpikun@gmail.com>
2022-12-09 22:58:00 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Palette } from "./Palette";
+import { Canvas } from "./Canvas";
+
+
+function App() {
+  return (
+    <div className="app">
+      <Palette />
+      <Canvas />
+      
+    </div>
+  );
+}
+
+export default App;
